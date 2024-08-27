55a24a9b94ea095f692e5922811901041c906434
Zafar Faraz <130891786+ZafarFaraz@users.noreply.github.com>
2024-08-27 21:25:50 +1000

    codesnippets

--- llama_flutter/lib/pages/ChatView.dart
+++ llama_flutter/lib/pages/ChatView.dart
@@ -156,95 +156,135 @@ class ChatView extends StatelessWidget {
     TextEditingController _controller = TextEditingController();
 
     return Container(
-        margin: EdgeInsets.all(20),
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.all(Radius.circular(20)),
-            boxShadow: [
-              BoxShadow(
-                  color: isDarkMode
-                      ? Colors.greenAccent.withAlpha(200)
-                      : Colors.greenAccent.withAlpha(250),
-                  blurRadius: 25.0,
-                  spreadRadius: 10.0,
-                  offset: const Offset(0.0, 0.0)),
-            ],
-            color: isDarkMode ? Colors.black : Colors.grey),
-        child: Padding(
-          padding: EdgeInsets.all(16),
-          child: Column(
-            children: [
-              Expanded(
-                child: ListView.builder(
-                  controller: scrollController, // Attach the scroll controller
-                  itemCount: chatHistory.length,
-                  itemBuilder: (context, index) {
-                    final message = chatHistory[index];
-                    final isUser = message['role'] == 'user';
-                    return Align(
-                      alignment:
-                          isUser ? Alignment.centerRight : Alignment.centerLeft,
-                      child: Container(
-                        padding: EdgeInsets.all(10),
-                        margin: EdgeInsets.all(15),
-                        decoration: BoxDecoration(
-                          color: isUser
-                              ? const Color.fromARGB(255, 36, 91, 37)
-                              : Colors.grey[800],
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        child: Text(message['content'] ?? ''),
-                      ),
-                    );
-                  },
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: TextField(
-                        controller: _controller,
-                        decoration: InputDecoration(
-                          hintText: 'Type a message',
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(30)),
-                        ),
-                        onSubmitted: (text) {
-                          if (text.isNotEmpty) {
-                            onSendMessage(text);
-                            _controller.clear();
-                            _scrollToBottom(); // Scroll to the bottom after submitting a message
-                          }
-                        },
-                      ),
-                    ),
-                    SizedBox(
-                      width: 20,
-                    ),
-                    CircleAvatar(
-                      backgroundColor: Colors.green,
-                      radius: 30,
-                      child: IconButton(
-                        icon: Icon(
-                          Icons.send,
-                          size: 30,
-                        ),
-                        onPressed: () {
-                          if (_controller.text.isNotEmpty) {
-                            onSendMessage(_controller.text);
-                            _controller.clear();
-                            _scrollToBottom(); // Scroll to the bottom after sending a message
-                          }
-                        },
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            ],
+      margin: EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(20)),
+        boxShadow: [
+          BoxShadow(
+            color: isDarkMode
+                ? Colors.greenAccent.withAlpha(200)
+                : Colors.greenAccent.withAlpha(250),
+            blurRadius: 25.0,
+            spreadRadius: 10.0,
+            offset: const Offset(0.0, 0.0),
           ),
-        ));
+        ],
+        color: isDarkMode ? Colors.black : Colors.grey,
+      ),
+      child: Padding(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                controller: scrollController, // Attach the scroll controller
+                itemCount: chatHistory.length,
+                itemBuilder: (context, index) {
+                  final message = chatHistory[index];
+                  final isUser = message['role'] == 'user';
+                  final content = message['content'] ?? '';
+
+                  return Align(
+                    alignment:
+                        isUser ? Alignment.centerRight : Alignment.centerLeft,
+                    child: Container(
+                      padding: EdgeInsets.all(10),
+                      margin: EdgeInsets.all(15),
+                      decoration: BoxDecoration(
+                        color: isUser
+                            ? const Color.fromARGB(255, 36, 91, 37)
+                            : Colors.grey[800],
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: _buildMessageContent(content),
+                    ),
+                  );
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: _controller,
+                      decoration: InputDecoration(
+                        hintText: 'Type a message',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                      ),
+                      onSubmitted: (text) {
+                        if (text.isNotEmpty) {
+                          onSendMessage(text);
+                          _controller.clear();
+                          _scrollToBottom(); // Scroll to the bottom after submitting a message
+                        }
+                      },
+                    ),
+                  ),
+                  SizedBox(width: 20),
+                  CircleAvatar(
+                    backgroundColor: Colors.green,
+                    radius: 30,
+                    child: IconButton(
+                      icon: Icon(Icons.send, size: 30),
+                      onPressed: () {
+                        if (_controller.text.isNotEmpty) {
+                          onSendMessage(_controller.text);
+                          _controller.clear();
+                          _scrollToBottom(); // Scroll to the bottom after sending a message
+                        }
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildMessageContent(String content) {
+    if (content.contains('```')) {
+      // Split the content on triple backticks to identify code blocks
+      final parts = content.split('```');
+      List<Widget> formattedContent = [];
+
+      for (int i = 0; i < parts.length; i++) {
+        if (i % 2 == 0) {
+          // Regular text
+          formattedContent.add(Text(parts[i]));
+        } else {
+          // Code block
+          formattedContent.add(Container(
+            width: double.infinity,
+            color: isDarkMode ? Colors.black54 : Colors.grey[300],
+            padding: EdgeInsets.all(8),
+            margin: EdgeInsets.symmetric(vertical: 5),
+            child: SelectableText(
+              parts[i],
+              style: TextStyle(
+                fontFamily: 'Courier',
+                backgroundColor: Colors.transparent,
+                color: isDarkMode ? Colors.greenAccent : Colors.black,
+              ),
+            ),
+          ));
+        }
+      }
+
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: formattedContent,
+      );
+    } else {
+      // Regular message without code block
+      return Text(content);
+    }
   }
 
   void _scrollToBottom() {
